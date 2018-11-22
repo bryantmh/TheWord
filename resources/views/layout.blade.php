ef7423e8e245487b9175e05d1b39e8b7e9063c5e
Bryant Hinton <bryanthinton01@gmail.com>
2018-11-21 20:57:12 -0700

    Basic template

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,43 @@
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>The Word</title>
+
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="{{asset('css/bootstrap.css')}}">
+        <link rel="stylesheet" href="{{asset('css/layout.css')}}">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+
+    </head>
+    <body>
+	 	<div class="containing-element">
+	 		<header>
+				<nav class="bg-dark static-top">
+					<div>
+						<h1><i class="fa fa-book"></i><a class="navbar-brand" href="#">The Word</a></h1>
+					</div>
+					<div class="navbar navbar-expand-md navbar-dark">
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navTabs" aria-controls="navTabs" aria-expanded="false" aria-label="Toggle navigation">
+							<span class="navbar-toggler-icon"></span>
+						</button>
+						<div class="collapse navbar-collapse" id="navTabs">
+							<div class="navbar-nav">
+								
+							</div>
+						</div>
+					</div>
+				</nav>
+			</header>
+	 		<div class="page-content" id="main-content" role="main">
+      			@yield('content')
+      		</div>
+    	</div>
+    </body>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jscroll/2.4.1/jquery.jscroll.min.js"></script>
+    <script src="{{asset('js/app.js')}}"></script>
+
+</html>
